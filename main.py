582e99030ac0143947fa748f6bb9f2ce432cd141
Anthony Aromin <aromin.anthony@e.ubaguio.edu>
2026-05-13 10:59:22 +0800

    feat: add function to handle raw value string

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from datetime import datetime
 import asyncio
 import uvicorn
+import re
 
 app = FastAPI()
 
@@ -26,6 +27,34 @@ async def example_hook(request: Request):
         print(f"[{received_at}] Device: {device} | Property: {prop} | Value: {raw_value} ")
 
     return { "status": "success" }
+
+def get_query_value(raw_value: str):
+    """
+    Gets the value from the raw_value property returned by Grafana. Use if the value cannot be found in A, and is complex.
+    Example usage:
+    value_string = alert.get('valueString', '')
+    raw_value = get_query_value(value_string)
+    handle_latency(id, raw_value)
+    """
+
+    blocks = re.findall(r"\[[^]]+]", raw_value)
+
+    processed_val = 0
+
+    for block in blocks:
+        if "type='query'" in block:
+            value_match = re.search(r"value=([\d.]+)", block)
+
+            if value_match:
+                raw_value = value_match.group(1)
+                clean_val = raw_value.strip("'\"")
+
+                try:
+                    processed_val = float(clean_val)
+                except ValueError:
+                    processed_val = clean_val
+
+                return processed_val
 
 
 if __name__ == "__main__":
